e6d32afbbdde5564b4b6288f7b8f6af73f45068e
markusporto <markusvinicius050@gmail.com>
2024-05-15 18:15:56 -0300

    Operadores

---
+++ TIPOS-VARIAVEIS/src/edu/markus/operadores/Operadores.java
@@ -0,0 +1,15 @@
+package edu.markus.operadores;
+public class Operadores {
+public static void main(String[] args) {
+    int numero = 5;
+    numero = - numero;
+
+    System.out.println(numero);
+
+    numero = numero * -1;
+
+    System.out.println(numero);
+
+
+}
+}
--- TIPOS-VARIAVEIS/src/edu/markus/variaveis/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/edu/markus/variaveis/TiposVariaveis.java
@@ -1,3 +1,4 @@
+package edu.markus.variaveis;
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
