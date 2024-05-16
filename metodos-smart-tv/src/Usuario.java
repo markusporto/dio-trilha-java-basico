16316ca866345e9efb55d8f6cf7ca25afc1bb2d1
markusporto <markusvinicius050@gmail.com>
2024-05-16 17:22:29 -0300

    Adicionando Metodos

---
+++ metodos-smart-tv/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ?" + smartTv.ligada);
+        System.out.println("Canal Atual :" + smartTv.canal);
+        System.out.println("Volume Atual :" + smartTv.volume);
+       
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual :" + smartTv.canal);
+        smartTv.mudarCanal(12);
+        System.out.println("Canal Atual :" + smartTv.canal);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual :" + smartTv.canal);
+    }
+}
